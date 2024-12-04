c6848edb3e15c594351c1e330fb2a06dbcf59e68
mohsenzzz <zareimohsen1@yahoo.com>
2024-12-04 22:25:05 +0330

    Add sms panel

--- SMSPanel/main.py
+++ SMSPanel/main.py
@@ -1,7 +1,7 @@
 from random import randint
 from datetime import datetime, timedelta
 
-from messageBroker import pop_value, push_value, phone_numbers, client
+from messageBroker import pop_value, push_value, phone_numbers, client,send_sms
 
 if __name__ == '__main__':
     print('start redis')
@@ -12,6 +12,11 @@ if __name__ == '__main__':
         code = randint(12345,98765)
         print(code)
         phone = phone[1].decode('utf-8')
-        push_value(phone,code)
+        try:
+            send_sms(phone, code)
+        except:
+            print(f'sms does not sent {code}')
+        finally:
+            push_value(phone,code)
       # value = pop_value('phone')
       # print(value)
--- SMSPanel/messageBroker.py
+++ SMSPanel/messageBroker.py
@@ -1,8 +1,11 @@
+import requests
+import json
+from config import *
 from redis import Redis
 
 
 
-client= Redis('192.168.56.16','6379')
+client= Redis(REDIS_IP,REDIS_PORT)
 phone_numbers = list()
 
 def push_value(queue, value):
@@ -18,4 +21,24 @@ def pop_value(queue):
         phone_numbers =client.blpop(queue)
         return phone_numbers
 
+def send_sms(phone_number, message):
+    url = "https://api2.ippanel.com/api/v1/sms/pattern/normal/send"
 
+    payload = json.dumps({
+        "code": "your-pattern-code",
+        "sender": "+983000505",
+        "recipient": phone_number,
+        "variable": {
+            "verification-code": "test"
+        },
+        "message": message
+
+    })
+    headers = {
+        'apikey': 'x_GJHLzs2h-s8dmURqX632dwWMC5YF0yO77Q2_YrWHw=',
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
